dff74c16a0046282b1c463663a041ec4525f8f5d
Lewis Scrivens <lewisscrivens1998@hotmail.com>
2018-09-18 13:37:02 +0100

    First commit. Adding third person template with imported assets from designers to make a Inverse Kinimatics demo showing how we can make third person movement work.

---
+++ Source/IKDEMO/IKDEMO.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "IKDEMO.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IKDEMO, "IKDEMO" );
+ 
